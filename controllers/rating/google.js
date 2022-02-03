803023f65f95077e3caeb12d1bfce78a9bf4887b
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-02-04 00:42:13 +0100

    deleted screenhoot

--- controllers/rating/google.js
+++ controllers/rating/google.js
@@ -104,7 +104,7 @@ const downloadGoogleReviewsHandle = async (selectedCompany, url, load) => {
 
 	const page = await usePuppeteer(url);
 	await page.waitForNetworkIdle();
-	await page.screenshot({path: './screenshoot/google.png'})
+	await page.screenshot({path: 'google.png'})
 	await page.click('button[aria-label*=review]');
 
 	const scrollableDiv = 'div.section-scrollbox';
