f0c129bfa1e807f19c75a37761270d4adf1bb7ec
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-02-04 00:33:09 +0100

    google added waitFor

--- controllers/rating/google.js
+++ controllers/rating/google.js
@@ -104,6 +104,7 @@ const downloadGoogleReviewsHandle = async (selectedCompany, url, load) => {
 
 	const page = await usePuppeteer(url);
 	await page.waitForNetworkIdle();
+	await page.waitFor(300);
 	await page.click('button[aria-label*=review]');
 
 	const scrollableDiv = 'div.section-scrollbox';
