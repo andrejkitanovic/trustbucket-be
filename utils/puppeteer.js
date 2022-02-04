9c886e38be4021702d62c1a52606fbf5c949a855
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-02-04 01:17:01 +0100

    updates to puppeteer

--- utils/puppeteer.js
+++ utils/puppeteer.js
@@ -1,15 +1,17 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 const options = {
 	// headless: false,
 	args: [
 		'--no-sandbox',
 		'--disable-setuid-sandbox',
-		'--disable-infobars',
-		'--window-position=0,0',
-		'--ignore-certifcate-errors',
-		'--ignore-certifcate-errors-spki-list',
-		'--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+		// '--disable-infobars',
+		// '--window-position=0,0',
+		// '--ignore-certifcate-errors',
+		// '--ignore-certifcate-errors-spki-list',
+		// '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
 	],
 };
 
