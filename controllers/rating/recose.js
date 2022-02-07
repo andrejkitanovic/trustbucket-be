6a1dfc1008bc33089cbef249e9f7be8b4a441c30
Andrej Kitanovic <andrej@ilder.io>
2022-02-07 12:02:34 +0100

    improved

--- controllers/rating/recose.js
+++ controllers/rating/recose.js
@@ -138,7 +138,7 @@ const downloadRecoseReviewsHandle = async (selectedCompany, url, load) => {
 			setTimeout(() => {
 				await page.waitForSelector('a.more-reviews-button');
 				await page.click('a.more-reviews-button');
-				loadMore();
+				await loadMore();
 			}, 3000);
 
 			// if (await page.$('a.more-reviews-button')) {
