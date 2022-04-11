af65213769fc28f9c12324c230ec868a242c6bde
Andrej Kitanovic <andrejkitanovic@Andrejs-MacBook-Pro.local>
2022-04-11 19:28:34 +0200

    less protection on webhook

--- controllers/webhook.js
+++ controllers/webhook.js
@@ -1,21 +1,21 @@
-const stripe = require('stripe')(process.env.STRIPE_PUBLISH_KEY);
-const endpointSecret = process.env.STRIPE_SECRET_KEY;
+// const stripe = require('stripe')(process.env.STRIPE_PUBLISH_KEY);
+// const endpointSecret = process.env.STRIPE_SECRET_KEY;
 
 exports.webhook = (req, res, next) => {
 	console.log('Webhook called', req.body);
 	let event = req.body;
 	// Only verify the event if you have an endpoint secret defined.
 	// Otherwise use the basic event deserialized with JSON.parse
-	if (endpointSecret) {
-		// Get the signature sent by Stripe
-		const signature = req.headers['stripe-signature'];
-		try {
-			event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
-		} catch (err) {
-			console.log(`⚠️ Webhook signature verification failed.`, err.message);
-			return res.sendStatus(400);
-		}
-	}
+	// if (endpointSecret) {
+	// 	// Get the signature sent by Stripe
+	// 	const signature = req.headers['stripe-signature'];
+	// 	try {
+	// 		event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
+	// 	} catch (err) {
+	// 		console.log(`⚠️ Webhook signature verification failed.`, err.message);
+	// 		return res.sendStatus(400);
+	// 	}
+	// }
 
 	// Handle the event
 	switch (event.type) {
