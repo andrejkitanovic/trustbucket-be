4f1e7d9f60af3bd72a1107feea908625c20143ed
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-04-13 22:12:30 +0200

    Added default email template

--- controllers/emailTemplate.js
+++ controllers/emailTemplate.js
@@ -1,8 +1,22 @@
 const EmailTemplate = require('../models/emailTemplate');
+const Company = require('../models/company');
+
+const defaultEmailTemplates = (companyName, slug) => [
+	{
+		content:
+			"\n<h1>Hi {firstName}</h1>\n<p></p>\n<p>Thank you so much for choosing us!</p>\n<p></p>\n<p>We truly appreciate every customer's opinion and we want to hear all about your experience with us at {companyName}</p>\n<p></p>\n<p>{review_link: Click here to submit your review}</p>\n<p></p>\n<p>Thank you for helping us,</p>\n<p>{firstNameofUser} at {companyName}</p>\n",
+		linkUrl: `https://reviews.trustbucket.io/${slug}`,
+		name: 'Review us on trustbucket',
+		subject: `How was your experience with ${companyName}`,
+		default: true,
+	},
+];
 
 exports.getEmailTemplates = async (req, res, next) => {
 	try {
 		const { selectedCompany } = req.auth;
+
+		const company = await Company.findById(selectedCompany);
 
 		const emailTemplates = await EmailTemplate.find({ company: selectedCompany }).select(
 			'name subject content linkUrl'
@@ -11,7 +25,7 @@ exports.getEmailTemplates = async (req, res, next) => {
 
 		res.status(200).json({
 			total: count,
-			data: emailTemplates,
+			data: [...emailTemplates, ...defaultEmailTemplates(company.name, company.slug)],
 		});
 	} catch (err) {
 		next(err);
