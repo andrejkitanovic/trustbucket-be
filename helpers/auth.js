a7be99ee4fc637ae2f95a6c431ba7ad066630998
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-04-13 17:13:07 +0200

    search for user

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,14 +1,24 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const auth = async (req, res, next) => {
 	try {
 		if (req.headers && req.headers.authorization) {
 			let authorization = req.headers.authorization.split(' ')[1];
 			let decoded = jwt.verify(authorization, process.env.DECODE_KEY);
+
+			const { id, type, selectedCompany } = decoded;
+
+			const user = await User.findById(user);
+
+			if (!user) {
+				res.status(401).send('User not found!');
+			}
+
 			req.auth = {
-				id: decoded.id,
-				type: decoded.type,
-				selectedCompany: decoded.selectedCompany,
+				id,
+				type,
+				selectedCompany,
 			};
 		}
 	} catch (err) {
