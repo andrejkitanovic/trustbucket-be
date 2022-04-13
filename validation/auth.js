f1bbb3930ec0315369ace089df5a897c186666b2
Andrej Kitanovic <andrej@ilder.io>
2022-04-13 14:43:06 +0200

    check for slug

--- validation/auth.js
+++ validation/auth.js
@@ -1,6 +1,7 @@
 const { body } = require('express-validator');
 const validation = require('../helpers/validation');
 const User = require('../models/user');
+const Company = require('../models/company');
 
 exports.updateEmail = [
 	body('newEmail', 'Email is required!').notEmpty().isEmail().normalizeEmail().withMessage('Email is not valid!'),
@@ -66,6 +67,18 @@ exports.register = [
 			return true;
 		}),
 	body('phone', 'Phone is required!').notEmpty(),
+	body('slug', 'Slug is required!')
+		.notEmpty()
+		.custom(async (value) => {
+			const slugExists = await Company.findOne({ slug: value });
+
+			if (Boolean(slugExists)) {
+				throw new Error('Slug is in use!');
+			}
+
+			return true;
+		}),
+
 	body('websiteURL', 'Website URL is required!').notEmpty().isURL().withMessage('Website URL is not valid!'),
 	validation,
 ];
