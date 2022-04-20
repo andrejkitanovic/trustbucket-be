6cad23f117848ac8db8a0e1b4835933eb0c775c5
Andrej Kitanovic <andrej@ilder.io>
2022-04-20 13:46:39 +0200

    Fix

--- controllers/webhook.js
+++ controllers/webhook.js
@@ -56,15 +56,12 @@ exports.webhook = async (req, res, next) => {
 			await company.save();
 			break;
 		case 'invoice.paid':
-			console.log(payment.billing_reason)
-			if (!payment.billing_reason === 'subscription_update') {
+			if (payment.billing_reason !== 'subscription_update') {
 				company.billingInfo.interval = payment.lines.data[0].plan.interval;
 				company.subscription.plan = parsedProducts[payment.lines.data[0].plan.id].product;
 				company.subscription.ends = new Date((payment.lines.data[0].period.end + 86400) * 1000);
 				company.subscription.id = payment.subscription;
 				company.billingInfo.vatNumber = payment.customer_tax_ids.length ? payment.customer_tax_ids[0].value : null;
-
-				console.log(company)
 
 				await company.save();
 			}
