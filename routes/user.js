2a07030f79db93c597b3c8b5015da6270cda8b80
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-04-22 23:40:55 +0200

    disabled delete route

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,6 @@ const router = express.Router()
 router.get('/', auth, userController.getUsers)
 router.post('/filter', auth, userController.filterUsers)
 // Should be auth admin
-router.delete('/', auth, userController.deleteUser)
+// router.delete('/', auth, userController.deleteUser)
 
 module.exports = router
