23ce618930f50e884e7ab673922a5f8d2f78bca8
Andrej Zerox <kitanovicandrej213@gmail.com>
2022-07-21 15:10:17 +0200

    logo

--- utils/emailTemplates/campaignEmail.js
+++ utils/emailTemplates/campaignEmail.js
@@ -211,7 +211,7 @@ module.exports = ({ content, button, color, logo, plan }) => `<!DOCTYPE html>
                                   object-fit: contain;
                                 "
                                   alt="Logo"
-                                  src=${logo}
+                                  src="${logo}"
                                 />
                               </td>
                             </tr>`
